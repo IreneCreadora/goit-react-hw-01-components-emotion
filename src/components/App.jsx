485a21f2ec2c2b76fbbd4816ddc569afae0a1391
IreneCreadora <nargiza.dancer@gmail.com>
2022-11-22 11:28:43 +0200

    added start files

---
+++ src/components/App.jsx
@@ -0,0 +1,26 @@
+import { Profile } from 'components/Profile/Profile';
+import { Statistics } from 'components/Statistics/Statistics';
+import { FriendList } from 'components/FriendList/FriendList';
+import { TransactionHistory } from 'components/TransactionHistory/TransactionHistory';
+
+import user from '../received/user';
+import data from '../received/data';
+import friends from '../received/friends';
+import transactions from '../received/transactions';
+
+export const App = () => {
+  return (
+    <>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
+    </>
+  );
+};
